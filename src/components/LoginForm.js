815919c0eb22566845767d889f55fb1331123e7c
KaiKeanaaina <kaikeanaaina@gmail.com>
2019-08-21 12:44:02 -1000

    textinput is able to change with onChange

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
+import {TextInput} from 'react-native';
 import {Button, Card, CardSection} from './common';
 
 class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {text: ' '};
 
   render() {
     return (
       <Card>
+        <CardSection>
+          <TextInput
+            value={this.state.value}
+            onChangeText={text => this.setState({text})}
+            style={{height: 20, width: 100}}
+          />
+        </CardSection>
         <CardSection />
-        <CardSection />
+
         <CardSection>
           <Button>Log In</Button>
         </CardSection>
