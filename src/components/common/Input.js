6f2f64df71e756351affa27c70a1e2cc7658bb9a
KaiKeanaaina <kaikeanaaina@gmail.com>
2019-08-22 12:18:21 -1000

    created a input handler

---
+++ src/components/common/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const Input =() => {
+ return(
+     <View>
+         <Text>
+             
+         </Text>
+     </View>
+ )
+}
+
+export {Input};
